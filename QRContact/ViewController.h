c6208108f529548dbf3045571b532f34709971c9
Takeshi Bingo <bingotakeshi@Takeshi-no-MacBook-Air.local>
2013-08-03 14:06:18 +0900

    Initial Commit

---
+++ QRContact/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  QRContact
+//
+//  Created by Takeshi Bingo on 2013/08/03.
+//  Copyright (c) 2013年 Takeshi Bingo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ QRContactTests/QRContactTests.h
@@ -0,0 +1,13 @@
+//
+//  QRContactTests.h
+//  QRContactTests
+//
+//  Created by Takeshi Bingo on 2013/08/03.
+//  Copyright (c) 2013年 Takeshi Bingo. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface QRContactTests : SenTestCase
+
+@end
